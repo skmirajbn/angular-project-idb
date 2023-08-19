aa9b2c4e3cea81240abf11178ed0a71fe5c93aca
skmirajbn <skmirajbn@gmail.com>
2023-08-19 15:15:04 +0600

    updated

--- frontend/src/app/web/services/get-data.service.ts
+++ frontend/src/app/web/services/get-data.service.ts
@@ -7,11 +7,19 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class GetDataService {
-  constructor(private _http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   //Getting The categories List
   getCategories(): Observable<any> {
     let getCategoriesEndpoint = '/categories/getCategories.php';
-    return this._http.get(environment.apiUrl + getCategoriesEndpoint);
+    return this.http.get(environment.apiUrl + getCategoriesEndpoint);
+  }
+
+  login(data: any) {
+    this.http.post(environment.apiUrl + '/login.php', { data }).subscribe({
+      next: function (res: any) {
+        localStorage.setItem('token', res.token);
+      },
+    });
   }
 }
